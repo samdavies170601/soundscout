efe4b11a581517149a2dd67295a82a75ae722e86
Sam Davies <samdavies170601@gmail.com>
2024-08-06 12:09:04 +0100

    Initial Commit

---
+++ soundscout/soundscoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  soundscoutApp.swift
+//  soundscout
+//
+//  Created by Sam Davies on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct soundscoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
