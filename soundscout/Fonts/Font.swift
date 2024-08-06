5ee67d967beb8acb1b0e7e43aa5cb04f0752ff85
Sam Davies <samdavies170601@gmail.com>
2024-08-06 12:26:27 +0100

    Add Custom Font (Inter) to the Project, Change File Structure

---
+++ soundscout/Fonts/Font.swift
@@ -0,0 +1,26 @@
+//
+//  Font.swift
+//  soundscout
+//
+//  Created by Sam Davies on 06/08/2024.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // Inter SemiBold
+    static public let interSemiBold30 = Font.custom("Inter-SemiBold", size: 30)
+    static public let interSemiBold16 = Font.custom("Inter-SemiBold", size: 16)
+    
+    // Inter Medium
+    static public let interMedium18 = Font.custom("Inter-Medium", size: 18)
+    static public let interMedium14 = Font.custom("Inter-Medium", size: 14)
+    
+    // Inter Regular
+    static public let interRegular14 = Font.custom("Inter-Regular", size: 14)
+    
+    // Inter Light
+    static public let interLight12 = Font.custom("Inter-Light", size: 12)
+    
+}
